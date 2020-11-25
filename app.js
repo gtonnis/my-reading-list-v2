bf3e2766dee077ce4e36f1fd6e0a5ac95bdbb9fc
Gregory Tonnis <61606412+gtonnis@users.noreply.github.com>
2020-11-25 13:36:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -161,4 +161,18 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
     });
 });
 
-//'<td><span class="glyphicon glyphicon-book"> Book-Information: </span></td>'  +
+ let btn1 = document.querySelector('#btn-1');
+            let btn2 = document.querySelector('#btn-2');
+            let btn3 = document.querySelector('#btn-3');
+            
+            btn1.addEventListener('click', () => {
+                document.body.style.backgroundImage = "url('images/old-books-1.jpg')";
+            });
+            
+             btn2.addEventListener('click', () => {
+                document.body.style.backgroundImage =  "url('images/dark-wood-3.jpg')";
+            });
+            
+             btn3.addEventListener('click', () => {
+                document.body.style.backgroundImage =   "url('images/library-1.jpg')";
+            });
